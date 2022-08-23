85b039bd4963e3029c9248de1a5bda0a0cb68de5
Andrea Dashe <andrea.r.dashe@gmail.com>
2022-08-22 19:55:51 -0700

    Completed function to generate random todos

--- mixed-messages.js
+++ mixed-messages.js
@@ -1,1 +1,16 @@
-console.log('hello world');
+const generateRandomNumber = () => {
+    return Math.floor(Math.random() * 6)
+}
+
+const verbs = ['Plant', 'Clean', 'Sweep', 'Write', 'Chase', 'Organize'];
+const nouns = ['herbs', 'the bathroom', 'dirt', 'notes', 'true love', 'books'];
+const details = ['in the rain', 'to the sounds of music', 'under the rug', 'in the moonlight', 'first thing in the morning', 'on the shelf'];
+
+const generateToDoList = () => {
+    first = generateRandomNumber();
+    second = generateRandomNumber();
+    third = generateRandomNumber();
+    console.log(verbs[first] + ' ' + nouns[second] + ' ' + details[third] + '.')
+}
+
+generateToDoList();
